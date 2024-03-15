db90c57940529c58b2c58beac7e5a78f8e702ea9
Ch0live <colive@scottlogic.com>
2024-03-15 10:21:44 +0000

    Remove Google and AWS options

--- chains.py
+++ chains.py
@@ -23,7 +23,6 @@ from langchain.prompts import (
 
 from typing import List, Any
 from utils import BaseLogger, extract_title_and_question
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 
 def load_embedding_model(embedding_model_name: str, logger=BaseLogger(), config={}):
@@ -37,16 +36,6 @@ def load_embedding_model(embedding_model_name: str, logger=BaseLogger(), config=
         embeddings = OpenAIEmbeddings()
         dimension = 1536
         logger.info("Embedding: Using OpenAI")
-    elif embedding_model_name == "aws":
-        embeddings = BedrockEmbeddings()
-        dimension = 1536
-        logger.info("Embedding: Using AWS")
-    elif embedding_model_name == "google-genai-embedding-001":        
-        embeddings = GoogleGenerativeAIEmbeddings(
-            model="models/embedding-001"
-        )
-        dimension = 768
-        logger.info("Embedding: Using Google Generative AI Embeddings")
     else:
         embeddings = SentenceTransformerEmbeddings(
             model_name="all-MiniLM-L6-v2", cache_folder="/embedding_model"
@@ -63,13 +52,6 @@ def load_llm(llm_name: str, logger=BaseLogger(), config={}):
     elif llm_name == "gpt-3.5":
         logger.info("LLM: Using GPT-3.5")
         return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
-    elif llm_name == "claudev2":
-        logger.info("LLM: ClaudeV2")
-        return BedrockChat(
-            model_id="anthropic.claude-v2",
-            model_kwargs={"temperature": 0.0, "max_tokens_to_sample": 1024},
-            streaming=True,
-        )
     elif len(llm_name):
         logger.info(f"LLM: Using Ollama: {llm_name}")
         return ChatOllama(
